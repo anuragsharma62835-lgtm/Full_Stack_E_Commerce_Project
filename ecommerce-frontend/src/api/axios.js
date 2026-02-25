d0a97375da586661c64ab1ae68933a6133a1aad2
Anurag Sharma <anuragsharma62835@gmail.com>
2026-02-26 02:10:57 +0530

    Updated API URL for deployment

--- ecommerce-frontend/src/api/axios.js
+++ ecommerce-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://full-stack-e-commerce-project-w8nn.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
