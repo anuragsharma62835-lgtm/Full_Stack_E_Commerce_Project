84ac5739e2c752f818da2eb84c2e86dccc1f53d5
Anurag Sharma <anuragsharma62835@gmail.com>
2026-02-26 02:36:15 +0530

    test deploy

--- ecommerce-backend/src/server.js
+++ ecommerce-backend/src/server.js
@@ -7,6 +7,7 @@ dotenv.config();
 connectDB();
 
 const PORT = process.env.PORT || 5000;
+console.log("NEW DEPLOY TEST");
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
