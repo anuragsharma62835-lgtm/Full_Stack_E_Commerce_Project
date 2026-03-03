1281a75e462f56f9184d94825051b788b1cf4d2b
Anurag Sharma <anuragsharma62835@gmail.com>
2026-03-04 00:59:10 +0530

    fix redirectURL

--- ecommerce-backend/src/routes/authRoutes.js
+++ ecommerce-backend/src/routes/authRoutes.js
@@ -30,12 +30,11 @@ router.get(
   passport.authenticate("google", { session: false }),
   (req, res) => {
     try {
-
       const token = req.user.token;
       const { email, name } = req.user.user;
 
-      const redirectURL = `http://localhost:5173/google-success?token=${token}&name=${name}&email=${email}`;
-
+      // const redirectURL = `http://localhost:5173/google-success?token=${token}&name=${name}&email=${email}`;
+      const redirectURL = `https://full-stack-e-commerce-project-pearl.vercel.app/google-success?token=${token}&name=${name}&email=${email}`;
       return res.redirect(redirectURL);
     } catch (error) {
       console.error(error);
