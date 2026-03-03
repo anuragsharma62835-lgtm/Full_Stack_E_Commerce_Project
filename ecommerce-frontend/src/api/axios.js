eb3d285b56d5980bbfbe111f4e5f0ebda76644ea
Anurag Sharma <anuragsharma62835@gmail.com>
2026-03-04 01:16:22 +0530

    trying

--- ecommerce-frontend/src/api/axios.js
+++ ecommerce-frontend/src/api/axios.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    window.location.hostname === "localhost"
-      ? "http://localhost:5000/api"
-      : "https://full-stack-e-commerce-project-w8nn.onrender.com/api",
+    // window.location.hostname === "localhost"
+    // ? "http://localhost:5000/api"
+    // :
+    "https://full-stack-e-commerce-project-w8nn.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
